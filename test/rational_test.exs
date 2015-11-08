13f103920132293c74370e15edd3d1dd9f03608c
Meredith Howard <mhoward@roomag.org>
2015-11-08 18:42:51 -0500

    Add tests for new

--- test/rational_test.exs
+++ test/rational_test.exs
@@ -1,4 +1,15 @@
 defmodule RationalTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Rational
+
+  alias Rational, as: R
+
+  test "Rational.new" do
+    assert R.new(2,3)   == %Rational{numer: 2, denom: 3}
+    assert R.new(2,3)   == R.new(6,9)
+    assert R.new(-6,9)  == %Rational{numer: -2, denom: 3}
+    assert R.new(-6,9)  == R.new(2,-3)
+    assert R.new(-6,-9) == R.new(2,3)
+  end
+
 end
